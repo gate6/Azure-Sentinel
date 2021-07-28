926c6fe5fbb09a47e5885ba575bb8a2a69c9da5a
Rajendra Khabiya <rajendra.khabiya@gate6.com>
2021-07-28 09:10:16 +0530

    Desc updated

--- Solutions/Lookout/Data Connectors/LookoutAPISentinelConnector/__init__.py
+++ Solutions/Lookout/Data Connectors/LookoutAPISentinelConnector/__init__.py
@@ -65,7 +65,8 @@ def post_data(body):
         'content-type': content_type,
         'Authorization': signature,
         'Log-Type': log_type,
-        'x-ms-date': rfc1123date
+        'x-ms-date': rfc1123date,
+        'time-generated-field' : 'eventTime'
     }
     response = requests.post(uri,data=body, headers=headers)
     if (response.status_code >= 200 and response.status_code <= 299):
